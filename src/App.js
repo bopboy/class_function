6734a4b2b1625fe4d2655d11010ee071a2a620a5
bopboy2 <scp0000@gmail.com>
2020-03-24 08:16:50 +0900

    05 class에lifecycle 메소드 사용

--- src/App.js
+++ src/App.js
@@ -40,13 +40,30 @@ function FuncComp(p) {
     </div>
   );
 }
-
+var classStyle = "color:red";
 class ClassComp extends React.Component {
   state = {
     number: this.props.initNumber,
     _date: new Date().toString()
   };
+  componentWillMount() {
+    console.log("%cclass => componentWillMount", classStyle);
+  }
+  componentDidMount() {
+    console.log("%cclass => componentDidMount", classStyle);
+  }
+  shouldComponentUpdate() {
+    console.log("%cclass => shouldComponentUpate", classStyle);
+    return true;
+  }
+  componentWillUpdate() {
+    console.log("%cclass => componentWillUpdate", classStyle);
+  }
+  componentDidUpdate() {
+    console.log("%cclass => componentDidUpdate", classStyle);
+  }
   render() {
+    console.log("%cclass => render", classStyle);
     return (
       <div className="container">
         <h2>class style component</h2>
