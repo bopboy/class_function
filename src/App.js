fe5ff557d8ba60ae091453bc6742bd16c0797c30
bopboy2 <scp0000@gmail.com>
2020-03-24 08:55:30 +0900

    07 함수에useEffect 사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
@@ -11,13 +11,22 @@ function App() {
   );
 }
 
+var funcStyle = "color:blue";
+var funcId = 0;
 function FuncComp(p) {
   var numberState = useState(p.initNumber);
   var number = numberState[0];
   var setNumber = numberState[1];
 
   var [_date, setDate] = useState(new Date().toString());
-
+  useEffect(function() {
+    console.log(
+      "%cfunc => useEffect (== componentDidMount & componentDidUpdate" +
+        ++funcId,
+      funcStyle
+    );
+  });
+  console.log("%cfunc => render" + ++funcId, funcStyle);
   return (
     <div className="container">
       <h2>function style component</h2>
